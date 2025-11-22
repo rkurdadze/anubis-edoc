3e994b39f2f5c2efeaa43179314ca7d2b3193af9
Roman Kurdadze <romankurdadze@gmail.com>
2025-11-22 17:34:10 +0400

    initial

---
+++ src/main/java/ge/comcom/anubis/edoc/AnubisEdocApplication.java
@@ -0,0 +1,13 @@
+package ge.comcom.anubis.edoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnubisEdocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnubisEdocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ge/comcom/anubis/edoc/AnubisEdocApplicationTests.java
@@ -0,0 +1,13 @@
+package ge.comcom.anubis.edoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnubisEdocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
